32a4fdd7b65440014445407fe84c125ffd289e31
Money <2932978634@qq.com>
2023-03-31 19:29:36 +0800

    获取vue组件实例

---
+++ vue/04.Vue3组件基础/01_product_demo/src/08.获取元素组件/App.vue
@@ -0,0 +1,40 @@
+<template>
+ <div class="app">
+  <h2 ref="title">title</h2>
+  <banner ref='banner'></banner>
+  <button ref="button" @click="butClick">button</button>
+ </div>
+</template>
+
+<script>
+import Banner from './Banner.vue';
+
+export default {
+components:{
+  Banner,
+},
+methods:{
+  butClick(){
+  console.log( 'title',this.$refs.title );
+  console.log( 'banner');
+  // 获取组件实例
+  console.log( this.$refs.banner );
+  this.$refs.banner.bannerClick();
+  console.log( 'button',this.$refs.button );
+  // 获取组件根元素，多个根元素则会返回其中第一个节点
+  console.log(this.$refs.banner.$el);
+  // 获取父组件实例
+  console.log(this.$parent);
+  // 获取自己的根组件
+  console.log(this.$root);
+
+  },
+},
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/04.Vue3组件基础/01_product_demo/src/08.获取元素组件/Banner.vue
@@ -0,0 +1,22 @@
+<template>
+ <h2>Bannner</h2>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      message: "Bannner",
+    }
+  },
+  methods:{
+    bannerClick(){
+    console.log('Bannner Clicked');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
