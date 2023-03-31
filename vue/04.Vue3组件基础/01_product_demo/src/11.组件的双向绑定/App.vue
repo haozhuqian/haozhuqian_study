c5a6e9a175665e56ef8b4f13be15d5f894b85f0e
Money <2932978634@qq.com>
2023-03-31 22:21:46 +0800

    Vue组件的数据双向绑定

---
+++ vue/04.Vue3组件基础/01_product_demo/src/11.组件的双向绑定/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app">
+    <home v-model="counter"></home>
+    <money :modelValue='counter' @update:modelValue="counter = $event"></money>
+    <button @click="decreament">-1</button>
+    <button @click="increament">+1</button>
+  </div>
+</template>
+
+<script>
+import Home from './Home.vue';
+import Money from './Money.vue';
+
+export default {
+  components: {
+    Home,
+    Money,
+  },
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+  methods: {
+    increament() {
+      this.counter += 1;
+    },
+    decreament() {
+      this.counter -= 1;
+    },
+  }
+}
+</script>
+
+<style scoped></style>
