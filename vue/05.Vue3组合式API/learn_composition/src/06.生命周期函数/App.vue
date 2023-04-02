20533ce5ac6bbd7057a2bcb6139fe4c18b8b9a8f
Money <2932978634@qq.com>
2023-04-02 18:41:33 +0800

    生命周期函数的注册

---
+++ vue/05.Vue3组合式API/learn_composition/src/06.生命周期函数/App.vue
@@ -0,0 +1,22 @@
+<template>
+ 
+</template>
+
+<script>
+import { onBeforeMount,onMounted,onUpdated,onBeforeUpdate,onBeforeUnmount,onUnmounted } from "vue";
+export default {
+  setup(){
+    onBeforeMount(()=>{
+      console.log('onBeforeMount');
+    })
+    onMounted(()=>{
+      console.log('onMounted');
+    });
+  
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
