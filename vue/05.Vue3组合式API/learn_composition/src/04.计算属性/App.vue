91b8421fa10d1dbb217b1087372c2c638687ae51
Money <2932978634@qq.com>
2023-04-02 17:41:54 +0800

    计算属性的使用

--- vue/05.Vue3组合式API/learn_composition/src/04.计算属性/App.vue
+++ vue/05.Vue3组合式API/learn_composition/src/04.计算属性/App.vue
@@ -1,9 +1,38 @@
 <template>
- 
+ <div class="App">
+  <h2>{{ fullName }}</h2>
+  <button @click="changeFullName">change name</button>
+ </div>
 </template>
 
 <script>
+import { computed,reactive } from 'vue';
+
 export default {
+  setup(){
+    const names = reactive({
+      firstName : 'Good',
+      lastName : 'Money',
+    }) 
+    const fullName = computed({
+      set(newname){
+        const tempName = newname.split(' ');
+        names.firstName = tempName[0];
+        names.lastName = tempName[1];
+      },
+      get(){
+        return names.firstName+' '+names.lastName;
+      }
+    })
+    const changeFullName = ()=>{
+      fullName.value='Hao ZhuQian';
+    }
+
+    return {
+      fullName,
+      changeFullName
+    }
+  }
   
 }
 </script>
