7b2919dd1df59bcde5cb444007da539e4c0d6d43
Money <2932978634@qq.com>
2023-04-02 17:21:51 +0800

    了解ref相关的其他API

---
+++ vue/05.Vue3组合式API/learn_composition/src/03.ref的其他API/App.vue
@@ -0,0 +1,36 @@
+<template>
+ <div class="App">
+  <h2>info : {{ name }} - {{ age }}-{{ is }}</h2>
+  <button @click = 'age++'>change age</button>
+  <button @click = 'is = "Hao"'>change is</button>
+ </div>
+</template>
+
+<script>
+import { reactive,toRefs,toRef,unref,isRef,shallowRef,triggerRef } from 'vue';
+
+export default {
+  setup(){
+    const info= reactive({
+      name:'Money',
+      age: 10,
+      is:'Good',
+    })
+    // 结构后失去响应式
+    // const { name,age } = info;
+    const { name,age } = toRefs(info) ;
+    const is = toRef(info,'is')
+
+    return {
+      name,
+      age,
+      is,
+    }
+  }
+  
+}
+</script>
+
+<style scoped>
+
+</style>
