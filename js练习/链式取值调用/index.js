db62be451729f77e27d7f1017ba7fa8f16e0ec4d
Money <2932978634@qq.com>
2024-06-22 18:13:39 +0800

    修改链式取值调用的实现

--- js练习/链式取值调用/index.js
+++ js练习/链式取值调用/index.js
@@ -2,47 +2,47 @@ const colors = ['red', 'green', 'blue', 'yellow', 'gray', 'black', 'white', 'cya
 const style = ['bold', 'underline', 'italic', 'dim', 'inverse', 'hidden', 'strikethrough'];
 //每次取值拿到一个新的函数对象，但是函数的原型是一样的，调用原型方法会再返回一个新的函数对象,并且拥有额外的属性。
 
-const createm = (styles={})=>{
-  const m = (s)=>({...styles,val:s});
-  // 这里更好的做法是不用m.s，而是用一个symbol来存储styles，这样就不会被外部修改了和随便访问了，同时最好也可以设置为不可遍历的键
-  m.s = styles;
-  Object.setPrototypeOf(m,pro);
+/**
+ * 创建一个新的函数对象，生成新的样式对象，添加到新的函数对象上
+ * @param styles 当前函数已有的样式
+ * @returns 
+ */
+const createm = (oldStyles, changeStyleFn) => {
+  const newStyles = { ...oldStyles };
+  if (changeStyleFn) changeStyleFn(newStyles);
+  const m = (s) => ({ ...newStyles, val: s });
+  m.s = newStyles;
+  Object.setPrototypeOf(m, pro);
   return m;
 }
 
-const getm = (m,fn)=>{
-  const style = {...m.s};
-  fn(style);
-  return createm(style);
-}
 
 const pro = {
-  color(color){
-    return getm(this,(style)=>style.color = color);
+  color(color) {
+    return createm(this, (style) => style.color = color);
   },
-  bg(color){
-    return getm(this,(style)=>style.bg = color);
+  bg(color) {
+    return createm(this, (style) => style.bg = color);
   },
-  style(s){
-    return getm(this,(style)=>style[s] = true);
+  style(s) {
+    return createm(this, (style) => style[s] = true);
   }
 };
 
-
-for(let c of colors){
+for (let c of colors) {
   Object.defineProperty(pro, c, {
     get() {
       return this.color(c);
     }
   });
-  Object.defineProperty(pro, 'bg'+c.replace(/^(.)/,(s)=>s.toUpperCase()), {
+  Object.defineProperty(pro, 'bg' + c.replace(/^(.)/, (s) => s.toUpperCase()), {
     get() {
       return this.bg(c);
     }
   });
 }
 
-for(let s of style){
+for (let s of style) {
   Object.defineProperty(pro, s, {
     get() {
       return this.style(s);
